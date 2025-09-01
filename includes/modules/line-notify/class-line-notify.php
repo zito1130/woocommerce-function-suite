db646773652f63a511b1c447935453d45e2a1087
zito <zito1130@outlook.com>
2025-09-01 11:10:07 +0800

    Feat: 新增傳送訂單資訊到官方 LINE

--- includes/modules/line-notify/class-line-notify.php
+++ includes/modules/line-notify/class-line-notify.php
@@ -30,21 +30,6 @@ class WFS_Line_Notify {
         $order = wc_get_order($order_id);
         if (!$order) return;
 
-        // 【*** 最終強化版：開始 ***】
-        // --- 步驟 1：定義一個清理函式，專門移除可能造成問題的字元 ---
-        if (!function_exists('wfs_clean_line_string')) {
-            function wfs_clean_line_string($string) {
-                // 先將 HTML entities 解碼 (例如 &amp; -> &)
-                $decoded_string = html_entity_decode($string, ENT_QUOTES, 'UTF-8');
-                // 移除所有 HTML 和 PHP 標籤
-                $no_tags_string = strip_tags($decoded_string);
-                // 將 Windows 的換行符 (\r\n) 和 Mac 的舊式換行符 (\r) 全部統一為標準的 \n
-                $normalized_newlines = str_replace(["\r\n", "\r"], "\n", $no_tags_string);
-                // 移除頭尾的空白和換行
-                return trim($normalized_newlines);
-            }
-        }
-
         // --- 步驟 2：準備所有原始資料 ---
         $store_name = $order->get_meta('_shipping_cvs_store_name');
         $raw_shipping_address = !empty($store_name) ? $store_name : $order->get_formatted_shipping_address();
@@ -53,23 +38,25 @@ class WFS_Line_Notify {
         $address_with_marker = str_replace(['<br/>', '<br />', '<br>'], '---WFS-LINEBREAK---', $raw_shipping_address);
 
         // --- 步驟 3：使用我們的清理函式，處理所有要放入訊息的變數 ---
-        $shipping_address_clean = str_replace('---WFS-LINEBREAK---', "\n", wfs_clean_line_string($address_with_marker));
+        $shipping_address_clean = str_replace('---WFS-LINEBREAK---', "\n", $address_with_marker);
         
-        $shipping_phone = wfs_clean_line_string($order->get_shipping_phone() ?: $order->get_billing_phone());
-        $customer_note = $order->get_customer_note() ? wfs_clean_line_string($order->get_customer_note()) : '無';
+        $shipping_phone = $order->get_shipping_phone() ?: $order->get_billing_phone();
+        $customer_note = $order->get_customer_note() ? $order->get_customer_note() : '無';
         
-        $line_name_raw = wfs_clean_line_string($order->get_meta('billing_line_id'));
+        $line_name_raw = $order->get_meta('billing_line_id');
         $line_id_text = !empty($line_name_raw) ? ' (LINE: ' . $line_name_raw . ')' : '';
 
-        $shipping_total_text = wfs_clean_line_string($order->get_shipping_total('raw') . ' ' . get_woocommerce_currency());
-        $total_text = wfs_clean_line_string($order->get_formatted_order_total());
+        $raw_symbol = get_woocommerce_currency_symbol();
+        $currency_symbol = html_entity_decode($raw_symbol);
+        $shipping_total_text = $order->get_shipping_total();
+        $total_text = $order->get_total();
         
-        $shipping_full_name = wfs_clean_line_string($order->get_formatted_shipping_full_name());
-        $billing_full_name = wfs_clean_line_string($order->get_formatted_billing_full_name());
-        $billing_phone = wfs_clean_line_string($order->get_billing_phone());
+        $shipping_full_name = $order->get_formatted_shipping_full_name();
+        $billing_full_name = $order->get_formatted_billing_full_name();
+        $billing_phone = $order->get_billing_phone();
 
         // --- 步驟 4：建立最終訊息字串 ---
-        $newline_placeholder = '||BR||';
+        $newline_placeholder = '|';
 
         // 步驟 2：建立訊息字串，但所有換行都使用替身符號
         $line_message  = "【訂單資訊】" . $newline_placeholder;
@@ -89,8 +76,8 @@ class WFS_Line_Notify {
         $line_message .= "聯絡電話: " . $billing_phone . $newline_placeholder;
         $line_message .= "------------------" . $newline_placeholder;
         $line_message .= "[金額]" . $newline_placeholder;
-        $line_message .= "運費: " . strip_tags(wc_price($order->get_shipping_total())) . $newline_placeholder;
-        $line_message .= "總額: " . $total_text . $newline_placeholder;
+        $line_message .= "運費: " . $currency_symbol . $shipping_total_text . $newline_placeholder;
+        $line_message .= "總額: " . $currency_symbol . $total_text . $newline_placeholder;
         $line_message .= "------------------" . $newline_placeholder;
         $line_message .= "[顧客備註]" . $newline_placeholder;
         $line_message .= $customer_note;
@@ -100,7 +87,7 @@ class WFS_Line_Notify {
 
         // 步驟 4：將已被編碼的「替身」，手動替換成換行符的編碼 "%0A"
         $final_message = str_replace(rawurlencode($newline_placeholder), '%0A', $encoded_message);
-        
+        $message_for_clipboard = str_replace($newline_placeholder, "\n", $line_message);
         // 步驟 5：建立最終的 URL
         $line_url = "https://line.me/R/oaMessage/" . esc_attr($this->line_oa_id) . "/" . $final_message;
         
@@ -124,7 +111,7 @@ class WFS_Line_Notify {
         
         <div class="wfs-resend-line-wrapper" id="wfsResendWrapper">
             <p>沒有看到傳送視窗嗎？或是不小心關掉了？</p>
-            <a href="<?php echo esc_url($line_url); ?>" class="button wfs-resend-line-button" id="wfsResendButton" target="_blank">點此手動傳送訂單資訊到 LINE</a>
+            <a href="<?php echo $line_url; ?>" class="button wfs-resend-line-button" id="wfsResendButton" target="_blank">點此手動傳送訂單資訊到 LINE</a>
         </div>
 
         <div class="wfs-modal-overlay" id="wfsOrderModal">
@@ -158,7 +145,7 @@ class WFS_Line_Notify {
                 var timestampKey = "wfs_order_timestamp_" + orderId;
                 var nonce = "<?php echo wp_create_nonce('wfs_line_notify_nonce'); ?>";
                 var lineURL = <?php echo json_encode($line_url); ?>;
-                var messageToCopy = <?php echo json_encode($line_message); ?>;
+                var messageToCopy = <?php echo json_encode($message_for_clipboard); ?>;
                 var thirtyMinutes = 30 * 60 * 1000;
                 var isProcessing = false;
 
@@ -232,7 +219,7 @@ class WFS_Line_Notify {
 
                 function confirmOrderAjax() {
                     // ★★★ 關鍵修正點：使用您指定的方式打開視窗 ★★★
-                    window.open(lineURL, "", "width=600,height=400,resizable=yes,scrollbars=yes");
+                    window.open("<?php echo $line_url; ?>", "", "width=600,height=400,resizable=yes,scrollbars=yes");
 
                     fetch('<?php echo esc_url(admin_url('admin-ajax.php')); ?>', {
                         method: 'POST',
